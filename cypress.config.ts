79f5b46b239e4a65b26bac98f7ad2792d2af69fe
hamzabla <hamzabessa19@gmail.com>
2024-08-31 12:12:07 +0200

    add comments

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,14 +5,14 @@ import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esb
 
 export default defineConfig({
   defaultCommandTimeout: 10000, // Adjust the timeout for commands
-  chromeWebSecurity: false, // some browsers handle iframe cross-origins differently than others
+  chromeWebSecurity: false, // Some browsers handle iframe cross-origins differently than others
   e2e: {
     baseUrl: process.env.CYPRESS_BASE_URL, // We used environment variable to handle the base url
     specPattern: 'cypress/e2e/**/*.feature', // Include .feature files for Cucumber
     fixturesFolder: 'fixtures',
     supportFile: 'cypress/support/e2e.ts',
-    video: true, // To record videos of tests
-    screenshotsFolder: 'cypress/screenshots', // Folder for screenshots
+    video: true, // To record videos of tests in case of failure
+    screenshotsFolder: 'cypress/screenshots', // Folder for screenshots, where we can have screenshot when the test fails
     videosFolder: 'cypress/videos', // Folder for videos
     reporter: 'cypress-multi-reporters', // Use multi reporters
     reporterOptions: {
